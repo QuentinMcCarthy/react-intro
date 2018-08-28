3124ae6854aeda8b92e00bd94eba2c811ed660f0
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-29 09:59:22 +1200

    HTML setup as Shopping List

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ class App extends Component {
 			<div>
 				<div className="jumbotron text-center">
 					<h1 className="display-4">Shopping List</h1>
+
+					<ul className="list-group">
+						<li className="list-group-item">Lorem Ipsum</li>
+						<li className="list-group-item">Dolor sit</li>
+						<li className="list-group-item">Amet consectetur</li>
+						<li className="list-group-item">Adipisciing</li>
+						<li className="list-group-item">Elit</li>
+					</ul>
+
+					<hr />
+
+					<div className="form-group">
+						<input type="text" className="form-control" id="listInput" />
+						<button type="submit" className="btn btn-primary w-100 mt-2">Add New Item</button>
+					</div>
 				</div>
 			</div>
 		)
